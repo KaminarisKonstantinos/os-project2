13e78bfd7090023e0111b78081b304d5c8fe56a9
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2024-01-10 15:15:20 +0200

    init

---
+++ project2_scheduler/scheduler/scheduler.c
@@ -0,0 +1,13 @@
+//###############################################################
+// 236064 1041756 Kaminaris Konstantinos
+//###############################################################
+#include <stdio.h>
+
+struct process {
+
+};
+
+int main(int argc, char **argv) {
+  printf("Implement the scheduler here!\n");
+  return 0;
+}
