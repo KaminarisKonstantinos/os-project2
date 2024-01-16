6ab6049622c0c5282eac18469d2f136c9cd20b74
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2024-01-16 18:39:47 +0200

    some progress

--- project2_scheduler/myscheduler/scheduler.c
+++ project2_scheduler/myscheduler/scheduler.c
@@ -4,13 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <time.h>
 
 #define NAMESIZE 50
-
-FILE *fp;
-char *line = NULL;
-size_t len = 0;
-ssize_t read;
+#define N_PROCESSES 50
 
 // A linked list node to store a process
 struct processNode {
@@ -25,7 +25,7 @@ struct queue {
 };
 
 // Create a new linked list node
-struct processNode* newProcess(char *name){
+struct processNode* newProcess(char *name, pid_t pid){
     struct processNode* temp 
         = (struct processNode*)malloc(sizeof(struct processNode));
     strcpy(temp->name, name);
@@ -43,10 +43,10 @@ struct queue* createQueue() {
 }
 
 // Enqueue a process
-void enqueue(struct queue* q, char *name){
+void enqueue(struct queue* q, char *name, pid_t pid){
 
     // Create new process node
-    struct processNode* temp = newProcess(name);
+    struct processNode* temp = newProcess(name, pid);
 
     // If queue is empty initialize head and tail
     if (q->tail == NULL) {
@@ -85,6 +85,13 @@ int main(int argc, char **argv) {
     printf("Implement the scheduler here!\n");
     printf("input: %s\n", argv[argc-1]); 
 
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t readline;
+    pid_t pid[N_PROCESSES];
+    int i;
+
     // Check input
     if (argc < 2)
         exit(EXIT_FAILURE);
@@ -97,22 +104,42 @@ int main(int argc, char **argv) {
     // Initiate queue
     struct queue* q = createQueue();
 
-    while ((read = getline(&line, &len, fp)) != -1) {
-        //printf("%s", line);
-        enqueue(q, line);
+    i = -1;
+    while ((readline = getline(&line, &len, fp)) != -1) {
+        i++;
+        //remove trailing newline
+        if (line[strlen(line) - 1] == '\n')
+            line[strlen(line) - 1] = '\0';
+        pid[i] = fork();
+        // fork gone wrong
+        if (pid[i] == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        // child
+        else if (pid[i] == 0) {
+            enqueue(q, line, getpid());
+            break;
+        }
+        // parent
+        else {
+        }
     }
 
     free(line);
+    fclose(fp);
 
-    struct processNode* i = q->head;
+    struct processNode* k = q->head;
 
     // Print entire queue
-    while (i != q->tail) {
-        printf("\n%s", i->name);
-        i = i->next;
+    while (k != q->tail) {
+        printf("\n%s", k->name);
+        k = k->next;
     }; 
 
     printf("\n%s", q->tail->name);
+
+    free(q);
 
     // TODO: Schedule
 
